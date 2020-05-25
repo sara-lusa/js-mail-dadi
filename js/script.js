fa32b7eda6520d4684fbde88268b571cb9fabf14
Sara <saracorsob@gmail.com>
2020-05-25 15:55:06 +0200

    Prima scrittura con commenti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert('Ciao');
+// Ho una lista di mail a disposizione
+var mail = ['esempiomail@gmail.com', 'mailesempio@hotmail.it', 'esempio.mail.nuovo@gmail.com', 'mail.esempio-nuovo@gmail.com'];
+var mailSingle;
+
+// Chiedo all'utente la sua mail
+var mailUtente = prompt('Qual è la tua mail?');
+
+// Ipotesi
+var mailPresenza = false;
+
+// Controllo della lista
+for ( var i = 0; i < mail.length; i++ ) {
+  // Se la sua mail è nella lista
+  if( mail[i] == mailUtente ) {
+    // Vero
+    mailPresenza = true;
+  }
+  
+console.log();
+
+// Cosa stampare a schermo nei due casi
+if ( mailPresenza ) {
+  document.getElementById('accesso').innerHTML = 'Accesso consentito: Benvenuto!';
+} else {
+  document.getElementById('accesso').innerHTML = 'Accesso negato.';
+}
