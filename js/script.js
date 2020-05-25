be8ed4f45e30fe39fd6e7bef7281392aec7f8320
Sara <saracorsob@gmail.com>
2020-05-25 17:24:11 +0200

    Seconda opzione gioco dadi

--- js/script.js
+++ js/script.js
@@ -1,39 +1,52 @@
-// Ho una lista di mail a disposizione
-var mail = ['esempiomail@gmail.com', 'mailesempio@hotmail.it', 'esempio.mail.nuovo@gmail.com', 'mail.esempio-nuovo@gmail.com'];
-var mailSingle;
-
-// Chiedo all'utente la sua mail
-var mailUtente = prompt('Qual è la tua mail?');
-console.log(mailUtente);
-
-// Ipotesi falsa
-var mailPresenza = false;
-
-// Controllo della lista
-for ( var i = 0; i < mail.length; i++ ) {
-  // Se la sua mail è nella lista
-  if( mail[i] == mailUtente ) {
-    // Ipotesi vera
-    mailPresenza = true;
-  }
-}
-
-// Cosa stampare a schermo per le due opzioni
-// Ipotesi vera
-if ( mailPresenza ) {
-  document.getElementById('accesso').innerHTML = 'Accesso consentito: Benvenuto!';
-}
-// Ipotesi rimane falsa
-else {
-  document.getElementById('accesso').innerHTML = 'Accesso negato.';
-}
+// // Ho una lista di mail a disposizione
+// var mail = ['esempiomail@gmail.com', 'mailesempio@hotmail.it', 'esempio.mail.nuovo@gmail.com', 'mail.esempio-nuovo@gmail.com'];
+// var mailSingle;
+//
+// // Chiedo all'utente la sua mail
+// var mailUtente = prompt('Qual è la tua mail?');
+// console.log(mailUtente);
+//
+// // Ipotesi falsa
+// var mailPresenza = false;
+//
+// // Controllo della lista
+// for ( var i = 0; i < mail.length; i++ ) {
+//   // Se la sua mail è nella lista
+//   if( mail[i] == mailUtente ) {
+//     // Ipotesi vera
+//     mailPresenza = true;
+//   }
+// }
+//
+// // Cosa stampare a schermo per le due opzioni
+// // Ipotesi vera
+// if ( mailPresenza ) {
+//   document.getElementById('accesso').innerHTML = 'Accesso consentito: Benvenuto!';
+// }
+// // Ipotesi rimane falsa
+// else {
+//   document.getElementById('accesso').innerHTML = 'Accesso negato.';
+// }
 
 // DADI
-// Numeri Random
-var numeroUtente = Math.floor(Math.random() * 6) + 1;
-var numeroComputer = Math.floor(Math.random() * 6) + 1;
 
-// Numeri visibili su screen
+// // Numeri Random
+// var numeroUtente = Math.floor(Math.random() * 6) + 1;
+// var numeroComputer = Math.floor(Math.random() * 6) + 1;
+//
+// // Numeri visibili su screen
+// document.getElementById('numero-utente').innerHTML = numeroUtente;
+// document.getElementById('numero-computer').innerHTML = numeroComputer;
+
+// Lista numeri utente
+var listaUtente = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+var listaComputer = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+console.log(listaUtente);
+console.log(listaComputer);
+
+// Somma e punteggio finale
+numeroUtente = listaUtente[0] + listaUtente[1] + listaUtente[2];
+numeroComputer = listaComputer[0] + listaComputer[1] + listaComputer[2];
 document.getElementById('numero-utente').innerHTML = numeroUtente;
 document.getElementById('numero-computer').innerHTML = numeroComputer;
 
